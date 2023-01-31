d3bd46d01ddde552a82e45193e76882478748e05
Cristian Bassotto <cristianbassotto21@gmail.com>
2023-01-31 17:22:43 +0100

    Filter

--- TwitterFiltering/src/main/java/upf/edu/filter/FileLanguageFilter.java
+++ TwitterFiltering/src/main/java/upf/edu/filter/FileLanguageFilter.java
@@ -1,11 +1,16 @@
 package upf.edu.filter;
 
-public class FileLanguageFilter {
+public class FileLanguageFilter implements LanguageFilter{
     final String inputFile;
     final String outputFile;
 
     public FileLanguageFilter(String inputFile, String outputFile) {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
+    }
+
+    @Override
+    public void filterLanguage(String language) throws Exception {
+
     }
 }
