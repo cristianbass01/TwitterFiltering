f5694750d526ac617be785beadb4003103bea2c3
Aus7e <92274266+Aus7e@users.noreply.github.com>
2023-01-31 20:11:16 +0100

    Simple Filtering

--- TwitterFiltering/src/main/java/upf/edu/parser/SimplifiedTweet.java
+++ TwitterFiltering/src/main/java/upf/edu/parser/SimplifiedTweet.java
@@ -2,6 +2,8 @@ package upf.edu.parser;
 
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.util.Optional;
@@ -9,40 +11,86 @@ import java.util.Optional;
 
 public class SimplifiedTweet {
 
-  // All classes use the same instance
-  private static JsonParser parser = new JsonParser();
+    // All classes use the same instance
+    private static JsonParser parser = new JsonParser();
 
-  private final long tweetId;			  // the id of the tweet ('id')
-  private final String text;              // the content of the tweet ('text')
-  private final long userId;			  // the user id ('user->id')
-  private final String userName;		  // the user name ('user'->'name')
-  private final String language;          // the language of a tweet ('lang')
-  private final long timestampMs;		  // seconduserIds from epoch ('timestamp_ms')
+    private final long tweetId;              // the id of the tweet ('id')
+    private final String text;              // the content of the tweet ('text')
+    private final long userId;              // the user id ('user->id')
+    private final String userName;          // the user name ('user'->'name')
+    private final String language;          // the language of a tweet ('lang')
+    private final long timestampMs;          // seconduserIds from epoch ('timestamp_ms')
 
-  public SimplifiedTweet(long tweetId, String text, long userId, String userName,
-                         String language, long timestampMs) {
-    this.tweetId = tweetId;
-    this.text = text;
-    this.userId = userId;
-    this.userName = userName;
-    this.language = language;
-    this.timestampMs = timestampMs;
-  }
+    public SimplifiedTweet(long tweetId, String text, long userId, String userName,
+                           String language, long timestampMs) {
+        this.tweetId = tweetId;
+        this.text = text;
+        this.userId = userId;
+        this.userName = userName;
+        this.language = language;
+        this.timestampMs = timestampMs;
+    }
 
-  /**
-   * Returns a {@link SimplifiedTweet} from a JSON String.
-   * If parsing fails, for any reason, return an {@link Optional#empty()}
-   *
-   * @param jsonStr
-   * @return an {@link Optional} of a {@link SimplifiedTweet}
-   */
-  public static Optional<SimplifiedTweet> fromJson(String jsonStr) {
-	  try {
-	        return Optional.ofNullable(new Gson().fromJson(jsonStr, SimplifiedTweet.class));
-	    } catch (Exception e) {
-	        return Optional.empty();
-	    }
-  }
+    /**
+     * Returns a {@link SimplifiedTweet} from a JSON String.
+     * If parsing fails, for any reason, return an {@link Optional#empty()}
+     *
+     * @param jsonStr
+     * @return an {@link Optional} of a {@link SimplifiedTweet}
+     */
+    public static Optional<SimplifiedTweet> fromJson(String jsonStr) {
+        long tweetId;              // the id of the tweet ('id')
+        String text;              // the content of the tweet ('text')
+        long userId;              // the user id ('user->id')
+        String userName;          // the user name ('user'->'name')
+        String language;          // the language of a tweet ('lang')
+        long timestampMs;          // seconduserIds from epoch ('timestamp_ms')
+
+        JsonElement je = JsonParser.parseString(jsonStr);
+        JsonObject jo = je.getAsJsonObject();
+
+
+        if (jo.has("id"))
+            tweetId = jo.get("id")
+                    .getAsLong();
+
+
+        if (jo.has("text"))
+                text = jo.get("text")
+                        .getAsString();
+
+
+
+        if (jo.has("user")) {
+            JsonObject userObj = jo.get("user").getAsJsonObject();
+
+
+            if (userObj.has("id")) {
+                userId = userObj.get("id")
+                        .getAsLong();
+
+            }
+
+            if (userObj.has("name"))
+            {
+                userName = userObj.get("name")
+                        .getAsString();
+
+            }
+        }
+        if (jo.has("language"))
+            language = jo.get("language")
+                    .getAsString();
+
+        if(jo.has("timestampMs"))
+            timestampMs = jo.get("timestampMs")
+                    .getAsLong();
+
+
+
+
+
+}
 
 
 
