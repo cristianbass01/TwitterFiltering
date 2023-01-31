9931087f111b1016cdbae73c022b340c3b89bbdf
Cristian <cristianbassotto21@gmail.com>
2023-01-31 18:05:13 +0100

    Auto stash before merge of "main" and "origin/main"

--- TwitterFiltering/src/main/java/upf/edu/filter/FileLanguageFilter.java
+++ TwitterFiltering/src/main/java/upf/edu/filter/FileLanguageFilter.java
@@ -1,12 +1,32 @@
 package upf.edu.filter;
 
+
+import upf.edu.parser.SimplifiedTweet;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Optional;
+
 public class FileLanguageFilter implements LanguageFilter{
     final String inputFile;
     final String outputFile;
 
-    public FileLanguageFilter(String inputFile, String outputFile) {
+
+    public FileLanguageFilter(String inputFile, String outputFile) throws IOException {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
+        FileReader reader = new FileReader(inputFile);
+        BufferedReader bReader = new BufferedReader(reader);
+
+        StringBuilder tweetJson = null;
+        while(bReader.ready()){
+            tweetJson.append(bReader.readLine());
+        }
+
+        Optional<SimplifiedTweet> sempTweet =
+
+
     }
 
     @Override
